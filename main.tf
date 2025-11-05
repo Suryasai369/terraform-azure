c03df602412194fcdac7662f1704efac448caa7c
Suryasai369 <19551a0535.suryasaii@gmail.com>
2025-11-06 00:45:37 +0530

    NSG Associated

--- main.tf
+++ main.tf
@@ -84,3 +84,7 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+resource "azurerm_subnet_network_security_group_association" "nsgattach" {
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  subnet_id = azurerm_subnet.subnet.id
+}
