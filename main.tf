97b559b6efe31ed5a41c6166f9ff432e35653465
Suryasai369 <19551a0535.suryasaii@gmail.com>
2025-11-05 17:37:32 +0530

    Add bootstrap configuration and variables for Azure provider

--- main.tf
+++ main.tf
@@ -21,6 +21,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -31,3 +35,4 @@ resource "azurerm_resource_group" "rg" {
     Team = "TF-team"
   }
 }
+
